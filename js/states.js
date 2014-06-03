dd6da348880621c5a88c71685603d4f5b4e787a4
Ludusamo <ludusamo@gmail.com>
2014-06-03 08:42:57 -0400

    Stuff

--- js/states.js
+++ js/states.js
@@ -8,28 +8,31 @@ var layer;
 Game.MainMenu = function(game) {};
 Game.MainMenu.prototype = {
 	preload:function() {
-		game.load.spritesheet('playerSheet', 'res/spritesheets/player.png', 16, 16);
+		game.load.spritesheet('playerSheet', 'res/spritesheets/player.png', 32, 32);
 		game.load.tilemap('testMap', 'res/maps/test.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('Tilesheet_A', 'res/spritesheets/Tilesheet_A.png');
 	},
 	create:function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.physics.arcade.gravity.y = 300;
+		game.physics.arcade.gravity.y = 1000;
 
 		map = game.add.tilemap('testMap');
 		map.addTilesetImage('Tilesheet_A', 'Tilesheet_A');
 		map.setCollisionBetween(2, 3);
 		layer = map.createLayer('layer');
-		// layer.resizeWorld();
+		layer.debug = true;
+		layer.resizeWorld();
 
 		player = game.add.sprite(0, 0, 'playerSheet');
 		game.physics.arcade.enable(player);
 		player.body.collideWorldBounds = true;
 
 		cursors = game.input.keyboard.createCursorKeys();
+
+		game.camera.follow(player);
 	},
 	update:function() {
-		// game.physics.arcade.collide(player, layer);
+		game.physics.arcade.collide(player, layer);
 
 		player.body.velocity.x = 0;
 		if (cursors.right.isDown) {
@@ -39,7 +42,7 @@ Game.MainMenu.prototype = {
 			player.body.velocity.x = -100;
 		}
 		if (cursors.up.isDown && player.body.onFloor()) {
-			player.body.velocity.y = -200;
+			player.body.velocity.y = -500;
 		}
 	}
 };
